a2ba433569afd2c7fa0d5b02afaa2890e0fffa9f
Lyubov Olekhova <olekhova@gmail.com>
2020-04-19 15:16:54 +0300

    add option --format

--- src/gendiff.js
+++ src/gendiff.js
@@ -5,4 +5,16 @@ program.version('0.0.1');
 
 program
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv);
+
+
+  // -f, --format [type]  output format
+
+  // program
+  // .version('0.1.0')
+  // .arguments('<cmd> [env]')
+  // .action(function (cmd, env) {
+  //   cmdValue = cmd;
+  //   envValue = env;
+  // });
