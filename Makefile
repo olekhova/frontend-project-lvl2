991418ccd7d388e86827c43a90c96b2b6cda997c
Lyubov Olekhova <olekhova@gmail.com>
2020-04-20 18:51:17 +0300

    add compaire flat JSON

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ publish:
 	npm publish --dry-run
 lint:
 	eslint .
+build-tests: 
+	npm run build-tests
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,5 +1,7 @@
-#!/usr/bin/env node
 import { Command } from 'commander';
+import fs from 'fs';
+import { has, reduce } from 'lodash';
+
 const program = new Command();
 program.version('0.0.1');
 
@@ -9,13 +11,38 @@ program
   .option('-f, --format [type]', 'output format')
   .parse(process.argv);
 
+const genDiff = (pathToFirstFile, pathToSecondFile) => {
+  const firstFile = JSON.parse(fs.readFileSync(pathToFirstFile));
+  const secondFile = JSON.parse(fs.readFileSync(pathToSecondFile));
+  //console.log (firstFile , secondFile);
+  // const keysOfFirstFile = Object.keys(firstFile);
+  // const keysOfSecondFile = Object.keys(secondFile);
+  // console.log (keysOfFirstFile, keysOfSecondFile);
+  const entriesOfFirstFile = Object.entries(firstFile);
+  const entriesOfSecondFile = Object.entries(secondFile);
 
-  // -f, --format [type]  output format
+  const result1 = reduce( entriesOfFirstFile, (acc, kv) => {
+    if (has(secondFile, kv[0]) && (kv[1] === secondFile[kv[0]])) {
+      return `${acc} \n   ${kv[0]} : ${kv[1]}`;
+    }
+    if (has(secondFile, kv[0]) && (kv[1] !== secondFile[kv[0]])) {
+      return `${acc} \n - ${kv[0]} : ${kv[1]} \n + ${kv[0]} : ${secondFile[kv[0]]}`;
+    }
+    if (!has(secondFile, kv[0])) {
+      return `${acc} \n - ${kv[0]} : ${kv[1]}`;
+    }
+    return acc;
+  }, '');
 
-  // program
-  // .version('0.1.0')
-  // .arguments('<cmd> [env]')
-  // .action(function (cmd, env) {
-  //   cmdValue = cmd;
-  //   envValue = env;
-  // });
+  const result = reduce( entriesOfSecondFile, (acc, kv) => {
+    if (!has(firstFile, kv[0])) {
+      return `${acc} \n + ${kv[0]} : ${kv[1]}`;
+    }
+    return acc;
+  }, result1);
+return `{${result} \n}`;
+};
+
+
+
+export default genDiff;
