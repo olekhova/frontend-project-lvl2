7dca4b715391c8c24a2e76b98e09370d7473162d
Lyubov Olekhova <olekhova@gmail.com>
2020-05-04 14:58:06 +0300

    update

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,43 +1,32 @@
 import genDiff from '../dist/index.js';
 
-test('genDiffJson', () => {
-  const pathToFile1 = `${__dirname}/../__fixtures__/before.json`;
-  const pathToFile2 = `${__dirname}/../__fixtures__/after.json`;
-  const result = genDiff(pathToFile1, pathToFile2);
-  expect(result).toEqual(`{
+const path = `${__dirname}/../__fixtures__`;
+const expected = `{
    host: hexlet.io
  - timeout: 50
  + timeout: 20
  - proxy: 123.234.53.22
  - follow: false
  + verbose: true
-}`);
+}`;
+
+test('genDiffJson', () => {
+  const pathToFile1 = `${path}/before.json`;
+  const pathToFile2 = `${path}/after.json`;
+  const result = genDiff(pathToFile1, pathToFile2);
+  expect(result).toEqual(`${expected}`);
 });
 
 test('genDiffYml', () => {
-  const pathToFile1 = `${__dirname}/../__fixtures__/before.yml`;
-  const pathToFile2 = `${__dirname}/../__fixtures__/after.yml`;
+  const pathToFile1 = `${path}/before.yml`;
+  const pathToFile2 = `${path}/after.yml`;
   const result = genDiff(pathToFile1, pathToFile2);
-  expect(result).toEqual(`{
-   host: hexlet.io
- - timeout: 50
- + timeout: 20
- - proxy: 123.234.53.22
- - follow: false
- + verbose: true
-}`);
+  expect(result).toEqual(`${expected}`);
 });
 
 test('genDiffIni', () => {
-  const pathToFile1 = `${__dirname}/../__fixtures__/before.ini`;
-  const pathToFile2 = `${__dirname}/../__fixtures__/after.ini`;
+  const pathToFile1 = `${path}/before.ini`;
+  const pathToFile2 = `${path}/after.ini`;
   const result = genDiff(pathToFile1, pathToFile2);
-  expect(result).toEqual(`{
-   host: hexlet.io
- - timeout: 50
- + timeout: 20
- - proxy: 123.234.53.22
- - follow: false
- + verbose: true
-}`);
+  expect(result).toEqual(`${expected}`);
 });
