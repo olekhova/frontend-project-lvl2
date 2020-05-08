9ac0208c7fb143ec853967a1a036195247002058
Lyubov Olekhova <olekhova@gmail.com>
2020-05-08 12:14:06 +0300

    add fixtures *Tree, add tests tree, add result.txt resultTree.txt

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,32 +1,23 @@
-import genDiff from '../dist/index.js';
+import path from 'path';
+import fs from 'fs';
+import genDiff from '../src/index.js';
 
-const path = `${__dirname}/../__fixtures__`;
-const expected = `{
-   host: hexlet.io
- - timeout: 50
- + timeout: 20
- - proxy: 123.234.53.22
- - follow: false
- + verbose: true
-}`;
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const fileBefore = getFixturePath('before');
+const fileAfter = getFixturePath('after');
+const fileBeforeTree = getFixturePath('beforeTree');
+const fileAfterTree = getFixturePath('afterTree');
+const configTypes = [['json'], ['yml'], ['ini']];
+const expected = String(fs.readFileSync(getFixturePath('result.txt')));
+const expectedTree = String(fs.readFileSync(getFixturePath('resultTree.txt')));
 
-test('genDiffJson', () => {
-  const pathToFile1 = `${path}/before.json`;
-  const pathToFile2 = `${path}/after.json`;
-  const result = genDiff(pathToFile1, pathToFile2);
-  expect(result).toEqual(`${expected}`);
-});
-
-test('genDiffYml', () => {
-  const pathToFile1 = `${path}/before.yml`;
-  const pathToFile2 = `${path}/after.yml`;
-  const result = genDiff(pathToFile1, pathToFile2);
-  expect(result).toEqual(`${expected}`);
-});
-
-test('genDiffIni', () => {
-  const pathToFile1 = `${path}/before.ini`;
-  const pathToFile2 = `${path}/after.ini`;
-  const result = genDiff(pathToFile1, pathToFile2);
-  expect(result).toEqual(`${expected}`);
+describe.each(configTypes)('gendiff %s', (configType) => {
+  test('plane', () => {
+    const result = genDiff(`${fileBefore}.${configType}`, `${fileAfter}.${configType}`);
+    expect(result).toEqual(expected);
+  });
+  test('tree', () => {
+    const result = genDiff(`${fileBeforeTree}.${configType}`, `${fileAfterTree}.${configType}`);
+    expect(result).toEqual(expectedTree);
+  });
 });
--- src/genDiff.js
+++ src/genDiff.js
@@ -7,28 +7,28 @@ const genDiff = (pathToFirstFile, pathToSecondFile) => {
   const firstFileContents = fs.readFileSync(pathToFirstFile);
   const firstFileExtName = path.extname(pathToFirstFile);
   const firstFile = getParser(firstFileExtName)(firstFileContents);
-  const secondFileCntents = fs.readFileSync(pathToSecondFile);
+  const secondFileContents = fs.readFileSync(pathToSecondFile);
   const secondFileExtName = path.extname(pathToSecondFile);
-  const secondFile = getParser(secondFileExtName)(secondFileCntents);
+  const secondFile = getParser(secondFileExtName)(secondFileContents);
   const entriesOfFirstFile = Object.entries(firstFile);
   const entriesOfSecondFile = Object.entries(secondFile);
 
   const result1 = reduce(entriesOfFirstFile, (acc, keyValue) => {
     if (has(secondFile, keyValue[0]) && (keyValue[1] === secondFile[keyValue[0]])) {
-      return `${acc}\n   ${keyValue[0]}: ${keyValue[1]}`;
+      return `${acc}\n    ${keyValue[0]}: ${keyValue[1]}`;
     }
     if (has(secondFile, keyValue[0]) && (keyValue[1] !== secondFile[keyValue[0]])) {
-      return `${acc}\n - ${keyValue[0]}: ${keyValue[1]}\n + ${keyValue[0]}: ${secondFile[keyValue[0]]}`;
+      return `${acc}\n  - ${keyValue[0]}: ${keyValue[1]}\n  + ${keyValue[0]}: ${secondFile[keyValue[0]]}`;
     }
     if (!has(secondFile, keyValue[0])) {
-      return `${acc}\n - ${keyValue[0]}: ${keyValue[1]}`;
+      return `${acc}\n  - ${keyValue[0]}: ${keyValue[1]}`;
     }
     return acc;
   }, '');
 
   const result = reduce(entriesOfSecondFile, (acc, keyValue) => {
     if (!has(firstFile, keyValue[0])) {
-      return `${acc}\n + ${keyValue[0]}: ${keyValue[1]}`;
+      return `${acc}\n  + ${keyValue[0]}: ${keyValue[1]}`;
     }
     return acc;
   }, result1);
