55cfe27378a413a462f18c19e102b51012299f0b
Lyubov Olekhova <olekhova@gmail.com>
2020-06-10 11:36:49 +0300

    remove compare.test, add fuctions areValuesNotObjects, areValuesEqual, isOneOfTheValuesObject in compare

--- src/compare.js
+++ src/compare.js
@@ -5,28 +5,36 @@ const createElementChanged = (type, name, valueBefore, valueAfter) => ({
   type, name, valueBefore, valueAfter,
 });
 const createElementChildren = (type, name, children) => ({ type, name, children });
+const isKeyInTwoObjects = (key, object1, object2) => {
+  if (has(object1, key) && has(object2, key)) return true;
+  return false;
+};
+const areValuesNotObjects = (value1, value2) => {
+  if (!isObject(value1) && !isObject(value2)) return true;
+  return false;
+};
+const areValuesEqual = (value1, value2) => value1 === value2;
+const isOneOfTheValuesObject = (value1, value2) => {
+  if ((!isObject(value1) && isObject(value2))
+   || (isObject(value1) && !isObject(value2))) return true;
+  return false;
+};
 
 const compare = (firstObject, secondObject) => {
   const keysOfFirstObject = Object.keys(firstObject);
   const keysOfSecondObject = Object.keys(secondObject);
   const keysOfTwoObject = union(keysOfFirstObject, keysOfSecondObject);
   const ast = keysOfTwoObject.map((key) => {
-    if (has(firstObject, key) && has(secondObject, key)) {
-      if (!isObject(firstObject[key]) && !isObject(secondObject[key])) {
-        if (firstObject[key] === secondObject[key]) {
-          return createElement('unchanged', key, firstObject[key]);
-        }
-        return createElementChanged('changed', key, firstObject[key], secondObject[key]);
+    if (isKeyInTwoObjects(key, firstObject, secondObject)) {
+      if (areValuesNotObjects(firstObject[key], secondObject[key])) {
+        return areValuesEqual(firstObject[key], secondObject[key])
+          ? createElement('unchanged', key, firstObject[key])
+          : createElementChanged('changed', key, firstObject[key], secondObject[key]);
       }
-      if ((!isObject(firstObject[key]) && isObject(secondObject[key]))
-        || (isObject(firstObject[key]) && !isObject(secondObject[key]))) {
-        return createElementChanged('changed', key, firstObject[key], secondObject[key]);
-      }
+      if (isOneOfTheValuesObject(firstObject[key], secondObject[key])) return createElementChanged('changed', key, firstObject[key], secondObject[key]);
       return createElementChildren('object', key, compare(firstObject[key], secondObject[key]));
     }
-    if (has(firstObject, key)) {
-      return createElement('deleted', key, firstObject[key]);
-    }
+    if (has(firstObject, key)) return createElement('deleted', key, firstObject[key]);
     return createElement('new', key, secondObject[key]);
   });
   return ast;
