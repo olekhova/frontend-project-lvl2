99acab8100f827bee4d2b345872ac261b2c65f7e
Lyubov Olekhova <olekhova@gmail.com>
2020-06-30 19:20:44 +0300

    fixed with the concept of depth

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,36 +1,35 @@
 import { isObject } from 'lodash';
 
-const getValue = (localValue, topIndent, indentStep) => {
+const getValue = (localValue, depth, indentStep) => {
   if (!isObject(localValue)) {
     return `${localValue}`;
   }
   const localKeys = Object.keys(localValue);
-  const newTopIndent = `${topIndent}${indentStep}`;
+  const indent = `${indentStep.repeat(depth * 2)}${indentStep.repeat(2)}`;
   const innerValue = localKeys
-    .map((key) => `${topIndent}${indentStep.repeat(3)}${key}: ${getValue(localValue[key], newTopIndent, indentStep)}`)
+    .map((key) => `${indent}${key}: ${getValue(localValue[key], depth + 1, indentStep)}`)
     .join('\n');
-  return `{\n${innerValue}\n${newTopIndent}}`;
+  return `{\n${innerValue}\n${indentStep.repeat(depth * 2)}}`;
 };
 
 const stylish = (diffList) => {
-  const iter = (localDiffList, topIndent, indentStep) => localDiffList
+  const iter = (localDiffList, depth, indentStep) => localDiffList
     .map((elem) => {
+      const indent = `${indentStep.repeat((depth - 1) * 2)}${indentStep}`;
       switch (elem.type) {
-        case 'unchanged': return `${topIndent}  ${elem.name}: ${getValue(elem.value, topIndent, indentStep)}`;
-        case 'changed': return `${topIndent}- ${elem.name}: ${getValue(elem.valueBefore, topIndent, indentStep)}\n${topIndent}+ ${elem.name}: ${getValue(elem.valueAfter, topIndent, indentStep)}`;
-        case 'deleted': return `${topIndent}- ${elem.name}: ${getValue(elem.value, topIndent, indentStep)}`;
-        case 'added': return `${topIndent}+ ${elem.name}: ${getValue(elem.value, topIndent, indentStep)}`;
+        case 'unchanged': return `${indent}  ${elem.name}: ${getValue(elem.value, depth, indentStep)}`;
+        case 'changed': return `${indent}- ${elem.name}: ${getValue(elem.valueBefore, depth, indentStep)}\n${indent}+ ${elem.name}: ${getValue(elem.valueAfter, depth, indentStep)}`;
+        case 'deleted': return `${indent}- ${elem.name}: ${getValue(elem.value, depth, indentStep)}`;
+        case 'added': return `${indent}+ ${elem.name}: ${getValue(elem.value, depth, indentStep)}`;
         case 'object': {
           const newDiffList = elem.children;
-          const newTopIndent = `${topIndent}${indentStep.repeat(2)}`;
-          return `${topIndent}  ${elem.name}: {\n${iter(newDiffList, newTopIndent, indentStep)}\n${topIndent}${indentStep}}`;
+          return `${indent}  ${elem.name}: {\n${iter(newDiffList, depth + 1, indentStep)}\n${indent}${indentStep}}`;
         }
         default:
           throw new Error(`Unknown element type: '${elem.type}'!`);
       }
     }).join('\n');
-  const result = `{\n${iter(diffList, '  ', '  ')}\n}`;
-  console.log(result);
+  const result = `{\n${iter(diffList, 1, '  ')}\n}`;
   return result;
 };
 
